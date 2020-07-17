00e3e44cb2cb201f7c0499d1d26c75d7f66f6ff7
tanjimTC <tanjimtc74@gmail.com>
2020-07-18 01:40:31 +0600

    Routes added for users

--- controllers/users.js
+++ controllers/users.js
@@ -11,6 +11,28 @@ module.exports = {
     const user = await newUser.save();
     res.status(201).json(user);
   },
+
+  getUser: async (req, res, next) => {
+    const { userID } = req.params;
+    const user = await User.findById(userID);
+    res.status(200).json(user);
+  },
+
+  replaceUser: async (req, res, next) => {
+    //Enforce and make sure req.body contains all the fields
+    const { userID } = req.params;
+    const newUser = req.body;
+    const result = await User.findByIdAndUpdate(userID, newUser);
+    res.status(200).json({ status: true });
+  },
+
+  updateUser: async (req, res, next) => {
+    //req.body may contains any number of fields
+    const { userID } = req.params;
+    const newUser = req.body;
+    const result = await User.findByIdAndUpdate(userID, newUser);
+    res.status(200).json({ status: true });
+  },
 };
 
 // we can interact with Mongoose in 3 different ways
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = require("express-promise-router")();
 const userController = require("../controllers/users");
+
+//  by "/" we are actually in /user route because app.js we used route as "app.use("/users", users);"
 router.route("/").get(userController.index).post(userController.newUser);
+
+//  by "/:userID" we are actually in "/user/:userID" route because app.js we used route as "app.use("/users", users);"
+router
+  .route("/:userID")
+  .get(userController.getUser)
+  .put(userController.replaceUser)
+  .patch(userController.updateUser);
 
 module.exports = router;
